41b6e64bbb1ea500b342ad535895117e11bb5e86
Andrey Spiridonov <spiand97@gmail.com>
2021-03-13 10:03:09 +0300

    model

--- homework/2-GAN/model.py
+++ homework/2-GAN/model.py
@@ -1,49 +1,331 @@
 import torch
+import wandb
 from torch import nn
-from .utils import compute_gradient_penalty, permute_labels
+from .utils import (permute_labels, calculate_fid, calculate_multilabel_accuracy,
+                    create_generator_inputs)
+import torch.nn.functional as F
+import pytorch_lightning as pl
+from torchvision.utils import make_grid
+import typing as tp
+import matplotlib.pyplot as plt
+from pytorch_lightning.loggers import WandbLogger
+
+
+class ResidualBlock(nn.Module):
+    """Residual Block with instance normalization."""
+
+    def __init__(self, dim_in, dim_out):
+        super(ResidualBlock, self).__init__()
+        self.main = nn.Sequential(
+            nn.Conv2d(dim_in, dim_out, kernel_size=3, stride=1, padding=1, bias=False),
+            nn.InstanceNorm2d(dim_out, affine=True, track_running_stats=True),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(dim_out, dim_out, kernel_size=3, stride=1, padding=1, bias=False),
+            nn.InstanceNorm2d(dim_out, affine=True, track_running_stats=True))
+
+    def forward(self, x):
+        return x + self.main(x)
+
 
 class Generator(nn.Module):
-    def __init__(self):
-        super().__init__()
-        # YOUR CODE
-        
-    def forward(self, x, labels):
-        # YOUR CODE
+    """Generator network."""
 
-        
-class Critic(nn.Module):
-    def __init__(self):
-        super().__init__()
-        # YOUR CODE
-        
+    def __init__(self, conv_dim=64, c_dim=5, repeat_num=6):
+        super(Generator, self).__init__()
+
+        layers = []
+        layers.append(nn.Conv2d(3 + c_dim, conv_dim, kernel_size=7, stride=1, padding=3, bias=False))
+        layers.append(nn.InstanceNorm2d(conv_dim, affine=True, track_running_stats=True))
+        layers.append(nn.ReLU(inplace=True))
+
+        # Down-sampling layers.
+        curr_dim = conv_dim
+        for i in range(2):
+            layers.append(nn.Conv2d(curr_dim, curr_dim * 2, kernel_size=4, stride=2, padding=1, bias=False))
+            layers.append(nn.InstanceNorm2d(curr_dim * 2, affine=True, track_running_stats=True))
+            layers.append(nn.ReLU(inplace=True))
+            curr_dim = curr_dim * 2
+
+        # Bottleneck layers.
+        for i in range(repeat_num):
+            layers.append(ResidualBlock(dim_in=curr_dim, dim_out=curr_dim))
+
+        # Up-sampling layers.
+        for i in range(2):
+            layers.append(nn.ConvTranspose2d(curr_dim, curr_dim // 2, kernel_size=4, stride=2, padding=1, bias=False))
+            layers.append(nn.InstanceNorm2d(curr_dim // 2, affine=True, track_running_stats=True))
+            layers.append(nn.ReLU(inplace=True))
+            curr_dim = curr_dim // 2
+
+        layers.append(nn.Conv2d(curr_dim, 3, kernel_size=7, stride=1, padding=3, bias=False))
+        layers.append(nn.Tanh())
+        self.main = nn.Sequential(*layers)
+
+    def forward(self, x, c):
+        # Replicate spatially and concatenate domain information.
+        # Note that this type of label conditioning does not work at all if we use reflection padding in Conv2d.
+        # This is because instance normalization ignores the shifting (or bias) effect.
+        c = c.view(c.size(0), c.size(1), 1, 1)
+        c = c.repeat(1, 1, x.size(2), x.size(3))
+        x = torch.cat([x, c], dim=1)
+        return self.main(x)
+
+
+class Discriminator(nn.Module):
+    """Discriminator network with PatchGAN."""
+
+    def __init__(self, image_size=128, conv_dim=64, c_dim=5, repeat_num=6):
+        super(Discriminator, self).__init__()
+        layers = []
+        layers.append(nn.Conv2d(3, conv_dim, kernel_size=4, stride=2, padding=1))
+        layers.append(nn.LeakyReLU(0.01))
+
+        curr_dim = conv_dim
+        for i in range(1, repeat_num):
+            layers.append(nn.Conv2d(curr_dim, curr_dim * 2, kernel_size=4, stride=2, padding=1))
+            layers.append(nn.LeakyReLU(0.01))
+            curr_dim = curr_dim * 2
+
+        kernel_size = int(image_size / 2 ** repeat_num)
+        self.main = nn.Sequential(*layers)
+        self.conv1 = nn.Conv2d(curr_dim, 1, kernel_size=3, stride=1, padding=1, bias=False)
+        self.conv2 = nn.Conv2d(curr_dim, c_dim, kernel_size=kernel_size, bias=False)
+
     def forward(self, x):
-        # YOUR CODE
+        h = self.main(x)
+        out_src = self.conv1(h)
+        out_cls = self.conv2(h)
+        return out_src, out_cls.view(out_cls.size(0), out_cls.size(1))
 
-        
-class StarGAN:
-    def __init__(self):
-        self.G = Generator()
-        self.D = Critic()
-        
-        # YOUR CODE
-        
-    def train(self):
-        self.G.train()
-        self.D.train()
-        
-    def eval(self):
-        self.G.eval()
-        self.D.eval()
 
-    def to(self, device):
-        self.D.to(device)
-        self.G.to(device)
-        
-    def trainG(self, image, label):
-        # YOUR CODE
-        
-    def trainD(self, image, label):
-        # YOUR CODE
+class VanillaStarGAN(pl.LightningModule):
 
-    def generate(self, image, label):
-        # YOUR CODE
+    def build_discriminator(self, image_shape: tp.Tuple[int]) -> nn.Module:
+        return Discriminator()
+
+    def build_generator(self, image_shape: tp.Tuple[int]) -> nn.Module:
+        return Generator()
+
+    def adversarial_loss(self, on_real_outputs: torch.Tensor,
+                         on_fake_outputs: tp.Optional[torch.Tensor] = None) -> torch.Tensor:
+        loss = - torch.mean(on_real_outputs)
+        if on_fake_outputs is not None:
+            loss += torch.mean(on_fake_outputs)
+        return loss
+
+    def classification_loss(self, predictions: torch.Tensor, labels: torch.Tensor) -> torch.Tensor:
+        return F.binary_cross_entropy_with_logits(predictions, labels)
+
+    def reconstruction_loss(self, reconstructed_images: torch.Tensor, original_images: torch.Tensor) -> torch.Tensor:
+        return torch.mean(torch.abs(original_images - reconstructed_images))
+
+    def gradient_norm(self, outputs, inputs):
+        gradient = torch.autograd.grad(outputs=outputs,
+                                       inputs=inputs,
+                                       grad_outputs=None,
+                                       retain_graph=True,
+                                       create_graph=True,
+                                       only_inputs=True)[0]
+        gradient = torch.flatten(gradient, start_dim=1)
+        return torch.sqrt(torch.sum(gradient ** 2, dim=1))
+
+    def gradient_penalty(self, original_images: torch.Tensor, fake_images: torch.Tensor) -> torch.Tensor:
+        alpha = torch.rand((original_images.shape[0], 1, 1, 1), device=self.device)
+        sampled_inputs = (alpha * original_images.data + (1 - alpha) * fake_images.data).requires_grad_()
+        discriminator_on_sampled_outputs, _ = self.discriminator(sampled_inputs)
+        gradient_norm = self.gradient_norm(discriminator_on_sampled_outputs, sampled_inputs)
+        return torch.mean((gradient_norm - 1) ** 2)
+
+    # @abstractmethod
+    def generator_loss(self, batch: tp.Tuple[torch.Tensor, torch.Tensor]) -> tp.Dict[str, tp.Any]:
+        inputs, labels = batch
+        permuted_labels = permute_labels(labels)
+
+        generator_outputs = self.generator(inputs, permuted_labels)
+        discriminator_on_fake_outputs, classification_on_fake_outputs = self.discriminator(generator_outputs)
+
+        adversarial_loss = self.adversarial_loss(on_real_outputs=discriminator_on_fake_outputs)
+        classification_loss = self.classification_loss(classification_on_fake_outputs, labels)
+
+        reconstructed_inputs = self.generator(generator_outputs, labels)
+        reconstruction_loss = self.reconstruction_loss(reconstructed_inputs, inputs)
+
+        loss = adversarial_loss + self.lambda_reconstruction * reconstruction_loss + self.lambda_classification * classification_loss
+
+        return {
+            'adversarial loss': adversarial_loss,
+            'classification loss': classification_loss,
+            'reconstruction loss': reconstruction_loss,
+            'loss': loss
+        }
+
+    # @abstractmethod
+    def discriminator_loss(self, batch: tp.Tuple[torch.Tensor, torch.Tensor]) -> tp.Dict[str, tp.Any]:
+        inputs, labels = batch
+        permuted_labels = permute_labels(labels)
+
+        discriminator_on_real_outputs, classification_on_real_outputs = self.discriminator(inputs)
+        classification_loss = self.classification_loss(classification_on_real_outputs, labels)
+
+        with torch.no_grad():
+            generator_outputs = self.generator(inputs, permuted_labels)
+        discriminator_on_fake_outputs, classification_on_fake_outputs = self.discriminator(generator_outputs)
+
+        adversarial_loss = self.adversarial_loss(discriminator_on_real_outputs, discriminator_on_fake_outputs)
+        gradient_penalty = self.gradient_penalty(inputs, generator_outputs)
+
+        loss = adversarial_loss + self.lambda_gradient_penalty * gradient_penalty + self.lambda_classification * classification_loss
+
+        return {
+            'adversarial loss': adversarial_loss,
+            'classification loss': classification_loss,
+            'gradient penalty': gradient_penalty,
+            'loss': loss
+        }
+
+    def __init__(
+            self,
+            control_images: torch.Tensor,
+            # original_labels: tp.List[int],
+            desired_labels: torch.Tensor,
+            label_names: tp.Dict[int, str],
+            discriminator_frequency: int,
+            lambda_reconstruction: float = 10.0,
+            lambda_classification: float = 1.0,
+            lambda_gradient_penalty: float = 10.0
+    ):
+        super(VanillaStarGAN, self).__init__()
+        self.discriminator_frequency = discriminator_frequency
+        self.control_images = control_images
+        # self.original_labels = original_labels
+        self.desired_labels = desired_labels
+        self.label_names = label_names
+        self.image_shape = control_images.shape[1:]
+        self.discriminator = self.build_discriminator(self.image_shape)
+        self.generator = self.build_generator(self.image_shape)
+        self.lambda_reconstruction = lambda_reconstruction
+        self.lambda_classification = lambda_classification
+        self.lambda_gradient_penalty = lambda_gradient_penalty
+
+        self.example_input_array = self.control_images
+
+    def on_validation_end(self) -> None:
+        fid_score = calculate_fid(self.generator, self.datamodule.val_dataloader)
+        discriminator_accuracy = calculate_multilabel_accuracy(self.discriminator, self.datamodule.val_dataloader)
+        control_images = self.generate_images(self.control_images, self.desired_labels)  # , self.original_labels
+        self.logger.experiment.log(
+            {
+                'fid_score': fid_score,
+                'accuracy': discriminator_accuracy,
+                'control_images': control_images
+            },
+            step=self.current_epoch,
+        )
+
+    def training_step(
+            self,
+            batch: tp.Tuple[torch.Tensor, torch.Tensor],
+            batch_idx: int,
+            optimizer_idx: int
+    ) -> tp.Dict[str, tp.Any]:
+        if optimizer_idx == 0:
+            return self.generator_loss(batch)
+        if optimizer_idx == 1 and batch_idx % self.discriminator_frequency:
+            return self.discriminator_loss(batch)
+
+    @torch.no_grad()
+    def generate_images(
+            self,
+            original_images: torch.Tensor,
+            desired_labels: torch.Tensor
+    ) -> wandb.Image:  # original_labels,
+        original_images = original_images.to(self.device)
+        batch_size, _, image_height, image_width = original_images.shape
+        n_transforms = desired_labels.shape[0] + 1
+        desired_labels = desired_labels.to(self.device)
+        generated_images = []
+        y_ticks_positions = [image_height // 2]
+        y_ticks_labels = ['original']
+        for label in desired_labels:
+            generated_images.append(
+                self.generator(
+                    original_images, label.unsqeeze(0).repeat((batch_size, 1))
+                )
+            )
+            y_ticks_positions.append(y_ticks_positions[-1] + image_height)
+            y_ticks_labels.append('\n'.join(self.label_names[i] for i, l in enumerate(label) if l > 0))
+
+        image_grid = make_grid(torch.cat(generated_images), nrow=n_transforms, normalize=True, scale_each=True)
+        plt.imshow(image_grid.permute(1, 2, 0).cpu().numpy())
+        plt.yticks(y_ticks_positions, y_ticks_labels)
+        plt.tick_params(
+            axis='y',
+            which='both',
+            left=False
+        )
+        plt.tick_params(
+            axis='x',
+            which='both',
+            bottom=False,
+            labelbottom=False
+        )
+        return wandb.Image(plt)
+
+# class VanillaStarGAN(StarGAN):
+#
+#     def build_discriminator(self, image_shape: tp.Tuple[int]) -> nn.Module:
+#         pass
+#
+#     def build_generator(self, image_shape: tp.Tuple[int]) -> nn.Module:
+#         pass
+#
+#     def generator_loss(self, batch: tp.Tuple[torch.Tensor, torch.Tensor]) -> tp.Dict[str, tp.Any]:
+#         pass
+#
+#     def discriminator_loss(self, batch: tp.Tuple[torch.Tensor, torch.Tensor]) -> tp.Dict[str, tp.Any]:
+#         pass
+# class Generator(nn.Module):
+#     def __init__(self):
+#         super().__init__()
+#         # YOUR CODE
+#
+#     def forward(self, x, labels):
+#         # YOUR CODE
+#
+#
+# class Critic(nn.Module):
+#     def __init__(self):
+#         super().__init__()
+#         # YOUR CODE
+#
+#     def forward(self, x):
+#         # YOUR CODE
+#
+#
+# class StarGAN:
+#     def __init__(self):
+#         self.G = Generator()
+#         self.D = Critic()
+#
+#         # YOUR CODE
+#
+#     def train(self):
+#         self.G.train()
+#         self.D.train()
+#
+#     def eval(self):
+#         self.G.eval()
+#         self.D.eval()
+#
+#     def to(self, device):
+#         self.D.to(device)
+#         self.G.to(device)
+#
+#     def trainG(self, image, label):
+#         # YOUR CODE
+#
+#     def trainD(self, image, label):
+#         # YOUR CODE
+#
+#     def generate(self, image, label):
+#         # YOUR CODE
--- homework/2-GAN/utils.py
+++ homework/2-GAN/utils.py
@@ -1,10 +1,162 @@
 import torch
 from torch import nn
+import pytorch_lightning as pl
+from torchvision.models.resnet import resnet101
+from torch.utils.data import DataLoader
+from pytorch_lightning.metrics.functional.accuracy import accuracy
+import typing as tp
 
 
-def compute_gradient_penalty(critic, real_samples, fake_samples):
-    # YOUR CODE
-    return gradient_penalty
+def permute_labels(
+        labels: torch.Tensor
+):
+    indices = torch.randperm(labels.shape[0])
+    return labels[indices]
 
-def permute_labels(labels):
-    # YOUR CODE
+
+def create_generator_inputs(
+        image_batch: torch.Tensor,
+        label_batch: torch.Tensor
+):
+    extra_shape = image_batch.shape[2:]
+    spatial_labels = label_batch.unsqueeze(-1).unsqueeze(-1).repeat(extra_shape)
+    return torch.cat((image_batch, spatial_labels), dim=1)
+
+
+def cov(
+        x: torch.Tensor,
+        x_mean: torch.Tensor = None
+) -> torch.Tensor:
+    dim = x.shape[-1]
+    if x_mean is None:
+        x_mean = x.mean(-1)
+    x = x - x_mean
+    return x @ x.T / (dim - 1)
+
+
+@torch.no_grad()
+def calculate_activation_statistics(
+        generator: nn.Module,
+        dataloader: DataLoader,
+        classifier: nn.Module
+) -> tp.Tuple[torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor]:
+    generator.eval()
+    classifier.eval()
+    # device = next(generator.parameters()).device
+
+    real_activations = []
+    fake_activations = []
+    for batch_x, batch_y in dataloader:
+        batch_y = permute_labels(batch_y)
+        # batch_x = batch_x.to(device)
+        real_activations.append(classifier.get_activations(batch_x))
+        fake_activations.append(classifier.get_activations(generator(batch_x, batch_y)))
+
+    real_activations = torch.vstack(real_activations)
+    fake_activations = torch.vstack(fake_activations)
+    real_activations_mean = torch.mean(real_activations, dim=0)
+    fake_activations_mean = torch.mean(fake_activations, dim=0)
+
+    real_activations_cov = cov(real_activations, real_activations_mean)
+    fake_activations_cov = cov(fake_activations, fake_activations_mean)
+    return real_activations_mean, real_activations_cov, fake_activations_mean, fake_activations_cov
+
+
+def calculate_frechet_distance(
+        mu1: torch.Tensor,
+        sigma1: torch.Tensor,
+        mu2: torch.Tensor,
+        sigma2: torch.Tensor,
+        eps: float = 1e-6
+) -> torch.Tensor:
+    """Numpy implementation of the Frechet Distance.
+    The Frechet distance between two multivariate Gaussians X_1 ~ N(mu_1, C_1)
+    and X_2 ~ N(mu_2, C_2) is
+            d^2 = ||mu_1 - mu_2||^2 + Tr(C_1 + C_2 - 2*sqrt(C_1*C_2)).
+
+    Stable version by Dougal J. Sutherland.
+
+    Params:
+    -- mu1   : Numpy array containing the activations of a layer of the
+               inception net (like returned by the function 'get_predictions')
+               for generated samples.
+    -- mu2   : The sample mean over activations, precalculated on an
+               representative data set.
+    -- sigma1: The covariance matrix over activations for generated samples.
+    -- sigma2: The covariance matrix over activations, precalculated on an
+               representative data set.
+
+    Returns:
+    --   : The Frechet Distance.
+    """
+
+    mu1 = torch.atleast_1d(mu1)
+    mu2 = torch.atleast_1d(mu2)
+
+    sigma1 = torch.atleast_2d(sigma1)
+    sigma2 = torch.atleast_2d(sigma2)
+
+    assert mu1.shape == mu2.shape, \
+        'Training and test mean vectors have different lengths'
+    assert sigma1.shape == sigma2.shape, \
+        'Training and test covariances have different dimensions'
+
+    diff = mu1 - mu2
+
+    #  + eps * torch.eye(sigma1.shape[0]).type_as(mu1)
+    eigenvals, _ = torch.eig(sigma1.dot(sigma2), eigenvectors=False)
+    tr_covmean = torch.sum(torch.sqrt(eigenvals[0]))
+
+    return (diff.dot(diff) + torch.trace(sigma1) +
+            torch.trace(sigma2) - 2 * tr_covmean)
+
+
+@torch.no_grad()
+def calculate_fid(
+        model: nn.Module,
+        dataloader: DataLoader,
+        support_model=None
+) -> float:
+    if support_model is None:
+        if not hasattr(calculate_fid, '_support_model'):
+            def get_activations(self, x):
+                x = self.conv1(x)
+                x = self.bn1(x)
+                x = self.relu(x)
+                x = self.maxpool(x)
+
+                x = self.layer1(x)
+                x = self.layer2(x)
+                x = self.layer3(x)
+                x = self.layer4(x)
+
+                x = self.avgpool(x)
+                x = torch.flatten(x, 1)
+
+                return x
+
+            model = resnet101(pretrained=True, progress=False)
+            model.get_activations = get_activations
+            setattr(calculate_fid, '_support_model', model)
+    else:
+        setattr(calculate_fid, '_support_model', support_model)
+
+    m1, s1, m2, s2 = calculate_activation_statistics(model, dataloader, getattr(calculate_fid, '_support_model'))
+    fid_value = calculate_frechet_distance(m1, s1, m2, s2)
+
+    return fid_value.item()
+
+
+def calculate_multilabel_accuracy(
+        model: nn.Module,
+        dataloader: DataLoader,
+) -> float:
+    predictions = []
+    targets = []
+    for batch_x, batch_y in dataloader:
+        # batch_x = batch_x.to(device)
+        predictions.append(model(batch_x))
+        targets.append(batch_y)
+    predictions = torch.vstack(predictions)
+    targets = torch.vstack(targets)
+    return accuracy(predictions, targets).item()
